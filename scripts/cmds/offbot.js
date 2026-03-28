8e9423a134e4bd521dd9696cc414e2dbc60ada25
xaiko-obito-007 <xaikojadid54@gmail.com>
2026-03-28 09:36:21 +0600

    Create offbot.js

---
+++ scripts/cmds/offbot.js
@@ -0,0 +1,20 @@
+module.exports = {
+  config: {
+    name: "offbot",
+    version: "1.0",
+    author: "MR.AYAN",
+    countDown: 45,
+    role: 0,
+    shortDescription: "Turn off bot",
+    longDescription: "Turn off bot",
+    category: "owner",
+    guide: "{p}{n}"
+  },
+  onStart: async function ({event, api}) {
+    const permission = [ "61581466845674" ];
+  if (!permission.includes(event.senderID)) {
+    api.sendMessage("╔════ஜ۩۞۩ஜ═══╗\nYou don't have permission to use this command.\n═══ஜ۩۞۩ஜ═══╝", event.threadID, event.messageID);
+    return;
+  }
+    api.sendMessage("╔════ஜ۩۞۩ஜ═══╗\nsuccessfully Turned Off System ✅\═══ஜ۩۞۩ஜ═══╝",event.threadID, () =>process.exit(0))}
+};
